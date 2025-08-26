22a9b1e743093f3c7423dda28b48236dbdb3f819
Tauhid Ahmed <tauhidahmed.dev@gmail.com>
2025-08-26 22:49:39 +0600

    add google calender

--- app/(dashboard)/dashboard/admin/calender/page.tsx
+++ app/(dashboard)/dashboard/admin/calender/page.tsx
@@ -1,3 +1,29 @@
-export default function adminCalenderPath() {
-  return <div>adminCalenderPath</div>;
+export default function AdminCalendarPage() {
+  return (
+    <div className="flex justify-center items-center p-6 bg-white min-h-screen">
+      {/* <iframe
+        src="https://calendar.google.com/calendar/appointments/schedules/AcZssZ2iW40L8jWIzQoVRnTxuJv_sLNW49dk6EuxQWCXAKc8EnuzW4D9oABKT4IRW-f6yG0xbwXGFwz3?gv=true"
+        style={{
+          border: "0",
+          backgroundColor: "transparent",
+        }}
+        width="100%"
+        height="800"
+        loading="lazy"
+        className="rounded-2xl"
+      /> */}
+      <iframe
+        src="https://calendar.google.com/calendar/embed?src=tauhidahmed.dev%40gmail.com&ctz=Asia%2FDhaka"
+        style={{
+          border: "0",
+          backgroundColor: "transparent",
+        }}
+        width="100%"
+        height="100%"
+        // frameborder="0"
+        scrolling="no"
+        className="h-screen bg-dark3"
+      ></iframe>
+    </div>
+  );
 }
